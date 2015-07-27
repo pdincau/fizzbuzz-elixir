89159e892ab5161f11bc438514d03329ef7407a9
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-07-27 14:16:43 +0200

    refactoring function definitions

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,19 +1,11 @@
 defmodule FizzBuzz do
 
-  def say(number) when rem(number, 15) == 0 do
-    "fizzbuzz"
-  end
+  def say(number) when rem(number, 15) == 0, do: "fizzbuzz"
 
-  def say(number) when rem(number, 3) == 0 do
-    "fizz"
-  end
+  def say(number) when rem(number, 3) == 0, do: "fizz"
 
-  def say(number) when rem(number, 5) == 0 do
-    "buzz"
-  end
+  def say(number) when rem(number, 5) == 0, do: "buzz"
 
-  def say(number) do
-    to_string(number)
-  end
+  def say(number), do: to_string(number)
 
 end
